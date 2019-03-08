97b2e5107b7a5da6e80b0c4c5768f27703f0855d
Louis Ritchie <louiscritchie@gmail.com>
2019-03-08 09:46:27 -0800

    get pingclient up to specification

--- pingclient.py
+++ pingclient.py
@@ -11,16 +11,15 @@ clientSocket = socket(AF_INET, SOCK_DGRAM)
 clientSocket.bind(('', 5001))
 clientSocket.settimeout(1)
 
-while True:
-	clientSocket.sendto("ping", ('', 5000))
+for i in range(10):
+        message = "ping" + " " + str(i + 1) + " " + str(time.localtime().tm_hour) + ":" + str(time.localtime().tm_min) + ":" + str(time.localtime().tm_sec)
+	clientSocket.sendto(message, ('', 5000))
         startTime = time.clock()
         try:
-            message, address = clientSocket.recvfrom(1024)
+            response, address = clientSocket.recvfrom(1024)
             responseTime = (time.clock() - startTime) * 1000
-            print message
+            print response
             sys.stdout.write("Total Time (seconds): ")
             print responseTime
         except:
             print "Request timed out"
-
-        time.sleep(1)
